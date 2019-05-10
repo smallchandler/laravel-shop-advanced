439e8f0dd89a2d1b4a4bc5abcfd0d31399d41a13
will <wenhaozh88@gmail.com>
2019-05-10 08:35:17 +0000

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,11 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
@@ -101,5 +103,10 @@ class Product extends Model
     public function scopeByIds($query, $ids)
     {
         return $query->whereIn('id', $ids)->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $ids)));
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 }
