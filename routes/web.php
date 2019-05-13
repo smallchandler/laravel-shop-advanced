518ef4d139cedab70ed6a68763bfb83d681c0f91
will <wenhaozh88@gmail.com>
2019-05-13 09:53:41 +0000

    减少秒杀接口 SQL 查询

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
+
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products', 'ProductsController@index')->name('products.index');
@@ -52,7 +54,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
     Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
 
-    Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 });
 // 后端回调不能放在 auth 中间件中
 Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
